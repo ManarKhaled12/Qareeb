2723f2951642db8d28ba1b761bf23718fe5e5bde
Mayadahisham5 <mayadahisham66@gmail.com>
2024-04-26 21:59:12 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,33 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
-import Header from './components/Header';
-
-
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={< Profile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
-   
-  )
+  );
 }
 
 
