f9b13680a2b08520b9ba5da757ff301cefd77908
habiba <habebabakr4@.com>
2024-04-26 05:10:05 +0300

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -13,3 +13,13 @@ app.use("/api/auth/", authRouter);
 app.listen(4000, () => {
   console.log('Example app listening on port 4000!');
 });
+
+app.use((err,req,res,next) =>{
+    const statusCode=err.statusCode||500;
+    const message=err.message||'Internal Server Error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode:statusCode,
+        message:message
+    });
+})
